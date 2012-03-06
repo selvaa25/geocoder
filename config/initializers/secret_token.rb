63a5076b8033b25bc1dcfca8c288f100a8bd73f4
therichelvam <therichelvam@railsfactory.org>
2012-03-06 18:37:02 +0530

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geocoder::Application.config.secret_token = '11ee46af3b29a201b00fa915f586065f4a79b42d15f6fc97569552fe4755398da4b45a9e26db08754ecd2e94dc0328ae8f271f1e0a016775b5602689789d75b9'
